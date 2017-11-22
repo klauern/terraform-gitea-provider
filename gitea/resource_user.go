8e0fdda083b464d62930c668f886d097ed5c3935
Nick Klauer <klauer@gmail.com>
2017-11-22 12:43:18 -0600

    add user create

--- gitea/resource_user.go
+++ gitea/resource_user.go
@@ -1,6 +1,8 @@
 package gitea
 
 import "github.com/hashicorp/terraform/helper/schema"
+import "code.gitea.io/sdk/gitea"
+import "github.com/pkg/errors"
 
 func resourceUser() *schema.Resource {
 	return &schema.Resource{
@@ -40,6 +42,11 @@ func resourceUser() *schema.Resource {
 				Optional:    true,
 				Description: "",
 			},
+			"password": {
+				Type:        schema.TypeString,
+				Optional:    false,
+				Description: "",
+			},
 			"is_admin_user": {
 				Type:        schema.TypeBool,
 				Optional:    true,
@@ -51,8 +58,29 @@ func resourceUser() *schema.Resource {
 }
 
 func resourceUserCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*gitea.Client)
+	create := gitea.CreateUserOption{
+		Email:      d.Get("email").(string),
+		FullName:   d.Get("full_name").(string),
+		LoginName:  d.Get("login").(string),
+		Password:   d.Get("password").(string),
+		SendNotify: false,
+		Username:   d.Get("username").(string),
+	}
+
+	user, err := client.AdminCreateUser(create)
+	if err != nil {
+		return errors.WithMessage(err, "unable to create user")
+	}
+
+	d.Set("id", user.ID)
+	d.Set("avatar_url", user.AvatarURL)
+	d.Set("username", user.UserName)
+	d.Set("email", user.Email)
+	d.Set("full_name", user.FullName)
 	return nil
 }
+
 func resourceUserRead(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
