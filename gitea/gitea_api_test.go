055aa93f3794ff8230b8cd69773d3cb67e9aa38f
Nick Klauer <klauer@gmail.com>
2017-11-24 14:04:46 -0600

    add other tests for Gitea API
    Add more tests to verify that the Gitea server works, or doesn't

--- gitea/gitea_api_test.go
+++ gitea/gitea_api_test.go
@@ -2,10 +2,22 @@ package gitea
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"code.gitea.io/sdk/gitea"
 )
+
+func TestGiteaUserDelete(t *testing.T) {
+	client := gitea.NewClient("http://localhost:3000", "978fd3ee4f71d6cf8a87556201d37293d719230d")
+	if err := client.AdminDeleteUser("random"); err != nil {
+		if strings.Contains(err.Error(), "404") {
+			t.Log("User already deleted")
+		} else {
+			t.Fatal(err)
+		}
+	}
+}
 
 func TestGiteaUserCreate(t *testing.T) {
 	client := gitea.NewClient("http://localhost:3000", "978fd3ee4f71d6cf8a87556201d37293d719230d")
@@ -21,4 +33,25 @@ func TestGiteaUserCreate(t *testing.T) {
 
 	fmt.Printf("%#v", user)
 
+}
+
+func TestGiteaUserEdit(t *testing.T) {
+	client := gitea.NewClient("http://localhost:3000", "978fd3ee4f71d6cf8a87556201d37293d719230d")
+	disabled := false
+	err := client.AdminEditUser("random", gitea.EditUserOption{
+		Active: &disabled,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGiteaUserRead(t *testing.T) {
+	client := gitea.NewClient("http://localhost:3000", "978fd3ee4f71d6cf8a87556201d37293d719230d")
+	user, err := client.GetUserInfo("random")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("%#v", user)
 }
