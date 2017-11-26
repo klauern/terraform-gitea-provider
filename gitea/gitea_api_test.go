00fafd872f799c27758118348b826b764491082b
Nick Klauer <klauer@gmail.com>
2017-11-26 15:21:55 -0600

    tweak gitea user create test

--- gitea/gitea_api_test.go
+++ gitea/gitea_api_test.go
@@ -22,9 +22,10 @@ func TestGiteaUserDelete(t *testing.T) {
 func TestGiteaUserCreate(t *testing.T) {
 	client := gitea.NewClient("http://localhost:3000", "978fd3ee4f71d6cf8a87556201d37293d719230d")
 	user, err := client.AdminCreateUser(gitea.CreateUserOption{
-		Email:     "random@example.com",
-		LoginName: "random",
-		Username:  "random",
+		Email:      "brewday@example.com",
+		LoginName:  "brewday",
+		Password:   "password",
+		SendNotify: false,
 	})
 
 	if err != nil {
